b0dd4b90dc0cac17315b8254c7b0f9311e819933
Kishor-Hegde <112265983+Kishor-Hegde@users.noreply.github.com>
2022-08-29 20:09:42 +0530

    Delete lib directory

--- lib/ScreenShot.py
+++
@@ -1,9 +0,0 @@
-import datetime,threading,pyautogui
-
-
-def takeScreenShot(intervel):
-    if not threading.main_thread().is_alive() : exit()
-    threading.Timer(intervel,takeScreenShot,args=[intervel]).start()
-    current = datetime.datetime.now()
-    myScreenshot = pyautogui.screenshot()
-    myScreenshot.save(f'{current.date()}_{str(current.time()).replace(":","-")}.png')
